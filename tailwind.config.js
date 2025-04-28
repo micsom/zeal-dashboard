7151fd3335e56160e1dcd47e992498ead70e8851
Michasel Sommer <i@michaelsommer.dev>
2025-04-28 07:53:17 +0200

    MS Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'royal-purple': '#AC7FF4',
+      },
+    },
+  },
+  plugins: [],
+}
